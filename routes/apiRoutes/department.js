3ec7d70cca8f88fb9e0d8060bc647e1403d83512
brotherson67 <mitchell.brotherson@gmail.com>
2021-12-12 01:51:17 -0700

    added two get methods to dep

--- routes/apiRoutes/department.js
+++ routes/apiRoutes/department.js
@@ -22,3 +22,37 @@ router.post("/departments", (req, res) => {
         });
     });
 });
+
+// GET METHODS 
+// ALL DEP
+router.get('/departments', (req, res) => {
+    const sql = `SELECT * FROM departments`;
+
+    db.query(sql, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: "You got all the departments",
+            data: row
+        });
+    });
+});
+
+// INDIVIDUAL DEPT
+router.get('/departments/:id', (req, res) => {
+    const sql = `` // make query that calls the roles taht are part of each department as well
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: "This is the dept you were wanting",
+            data: row
+        });
+    });
+});
