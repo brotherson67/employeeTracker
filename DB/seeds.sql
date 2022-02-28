a6b35b3b5a9172c49e9af48b112e97338d9b214a
brotherson67 <mitchell.brotherson@gmail.com>
2022-02-27 20:16:35 -0700

    updated seeds to better reflect schema

--- DB/seeds.sql
+++ DB/seeds.sql
@@ -6,11 +6,17 @@ VALUES
     ("C-Class"),
     ("Development");
 
-INSERT INTO employees(first_name, last_name)
+INSERT INTO roles (role_name, role_description, dep_id)
+VALUES 
+    ("solutions archectect", "this is a job", 2),
+    ("head honcho", "hes a leader", 3),
+    ("sales lead", "hell sell you anything", 1)
+
+INSERT INTO employees(first_name, last_name, role_id)
 VALUES
-    ('james', 'Fraser'),
-    ('Jack', 'London'),
-    ('Robert', 'Bruce'),
-    ('Peter', 'Greenaway'),
-    ('Derek', 'Jarman'),
-    ('Paolo', 'Pasolini');
+    ('james', 'Fraser', 1),
+    ('Jack', 'London', 2),
+    ('Robert', 'Bruce', 3),
+    ('Peter', 'Greenaway', 2),
+    ('Derek', 'Jarman', 1),
+    ('Paolo', 'Pasolini', 2);
