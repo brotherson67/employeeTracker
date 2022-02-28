1cf0c6899b4afdd490ef1452c0de868ba3d5b923
brotherson67 <mitchell.brotherson@gmail.com>
2022-02-27 20:40:34 -0700

    got arrows to work

--- DB/dbConnection.js
+++ DB/dbConnection.js
@@ -1,16 +1,16 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 // Connect to DB
 const db = mysql.createConnection(
-    {
-        host: 'localhost', 
-        // Your mysql username
-        user: 'root', 
-        // your mysql password
-        password: 'SQLPassword97',
-        database: 'election'
-    },
-    console.log('Connected to the election database.')
+  {
+    host: "localhost",
+    // Your mysql username
+    user: "root",
+    // your mysql password
+    password: "SQLPassword97",
+    database: "election",
+  },
+  console.log("Connected to the election database.")
 );
 
 module.exports = db;
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const inquirer = require('inquirer');
+const express = require("express");
+const inquirer = require("inquirer");
 // const inputCheck = require('./utils/inputCheck');
-const db = require('./db/dbConnection');
-const apiRoutes = require('./routes/apiRoutes');
+const db = require("./db/dbConnection");
+const apiRoutes = require("./routes/apiRoutes");
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -12,46 +12,57 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // USE apiRoutes
-app.use('/api', apiRoutes);
-
-inquirer
+app.use("/api", apiRoutes);
+firstPrompt = () => {
+  inquirer
     .prompt([
-        {
-            name: "choice",
-            type: "list",
-            message: "What would you like to do?",
-            choices: ["View departments", 'View all roles', "View all employees", 'Add a department', 'Add a role', 'Add an employee', 'Update an employees role']
-        }
+      {
+        name: "choice",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "View departments",
+          // new inquirer.Separator(),
+          "View all roles",
+          "View all employees",
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Update an employees role",
+        ],
+      },
     ])
-    .then(answers => {
-        console.log(answers);
-        const choice = answers.choices;
-        if (choice === "View departments"){
-            console.log("they want to view the dept");
-            router.get('/departments', (req, res) => {
-                db.query(`SELECT * FROM department`, (err, result) => {
-                    if (err) {
-                        console.log('Sorry, looks like there was an error getting that.')
-                        res.status(500).json({ error: err.message });
-                        return;
-                    }
-                    res.json({
-                        message: 'Nice, you got the departments',
-                        result: body
-                    })
-                });
-            })
-        }
-         
-    })
+    .then((answer) => {
+      console.log(answer);
+      // console.log(answers);
+      // const choice = answers.choices;
+      // if (choice === "View departments") {
+      //   console.log("they want to view the dept");
+      //   router.get("/departments", (req, res) => {
+      //     db.query(`SELECT * FROM department`, (err, result) => {
+      //       if (err) {
+      //         console.log("Sorry, looks like there was an error getting that.");
+      //         res.status(500).json({ error: err.message });
+      //         return;
+      //       }
+      //       res.json({
+      //         message: "Nice, you got the departments",
+      //         result: body,
+      //       });
+      //     });
+      //   });
+      // }
+    });
+};
+firstPrompt();
 
 // START SERVER AFTER DATABASE CONNECTION
-db.connect(err => {
-    if (err) throw err;
-    console.log('You\'ve connected to the database.');
+db.connect((err) => {
+  if (err) throw err;
+  //   console.log("You've connected to the database.");
 
-    // CONNECTION FUNCTION
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
+  // // CONNECTION FUNCTION
+  // app.listen(PORT, () => {
+  //     console.log(`Server running on port ${PORT}`);
+  // });
 });
