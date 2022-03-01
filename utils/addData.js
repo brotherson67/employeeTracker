af2b487a84cbd5f6c113c72ae89b48c70be2f9f8
brotherson67 <mitchell.brotherson@gmail.com>
2022-03-01 01:23:59 -0700

    fixed syntax error in addData

--- utils/addData.js
+++ utils/addData.js
@@ -17,4 +17,13 @@ const newRole = (obj) => {
   });
 };
 
-module.exports = { newDept, newRole };
+const newEmployee = (obj) => {
+  const sql = `INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)`;
+  const params = [obj.first_name, obj.last_name, obj.role_id, obj.manager_id];
+  db.query(sql, params, (err, res) => {
+    if (err) throw err;
+    return;
+  });
+};
+
+module.exports = { newDept, newRole, newEmployee };
