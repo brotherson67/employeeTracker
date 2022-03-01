239fea4fa6a2adaf632a20f290d5b67d56911fc8
brotherson67 <mitchell.brotherson@gmail.com>
2022-03-01 02:00:44 -0700

    removed old inquirer function

--- server.js
+++ server.js
@@ -36,46 +36,6 @@ let employees = getEmployees();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// USE apiRoutes
-app.use("/api", apiRoutes);
-firstPrompt = () => {
-  inquirer
-    .prompt([
-      {
-        name: "choice",
-        type: "list",
-        message: "What would you like to do?",
-        choices: [
-          "View departments",
-          "View all roles",
-          "View all employees",
-          "Add a department",
-          "Add a role",
-          "Add an employee",
-          "Update an employees role",
-        ],
-      },
-    ])
-    .then((answer) => {
-      return answer;
-    });
-};
-const test = () => {
-  firstPrompt().then((answer) => {
-    switch (answer.choice) {
-      case "View departments":
-        console.table(getDepts);
-        return;
-      case "View all roles":
-        console.table(getRoles);
-        return;
-      case "View all employees":
-        console.table(getEmployees);
-        return;
-    }
-  });
-};
-
 // START SERVER AFTER DATABASE CONNECTION
 db.connect((err) => {
   if (err) throw err;
