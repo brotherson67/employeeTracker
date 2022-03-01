daf37eb034e0ae3c473e86770869a25843b0b30f
brotherson67 <mitchell.brotherson@gmail.com>
2022-03-01 02:19:26 -0700

    added funcitonality to new role

--- server.js
+++ server.js
@@ -100,6 +100,14 @@ const addRole = () => {
         type: "input",
         name: "salary",
         message: "What is the salary of the role?",
+        validate: function (salary) {
+          if (!isNaN(salary)) {
+            return true;
+          } else {
+            console.log("You must enter a salary number!");
+            return false;
+          }
+        },
       },
       {
         type: "list",
@@ -108,7 +116,14 @@ const addRole = () => {
         choices: deptArr,
       },
     ])
-    .then((ans) => {});
+    .then((ans) => {
+      const role = new Roles(ans.title, ans.salary, ans.department);
+      newRole(role);
+      console.log("Role Added!");
+      roles = getRoles();
+      roleArr = roleArrFill();
+      return init();
+    });
 };
 
 const PORT = process.env.PORT || 3000;
